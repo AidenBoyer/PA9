7d2a8d50bee262a527cc3a6c9a4a47a5226218c1
AVanCorbach-nav <a.vancorbach@wsu.edu>
2025-12-01 16:50:49 -0800

    Update to main.cpp
    Added code to allow for a fixed ratio of 16:9 to be enforced when resizing the window.

--- PA9/PongSFML_3_0/main.cpp
+++ PA9/PongSFML_3_0/main.cpp
@@ -6,7 +6,8 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "Space Defender");
-
+    
+    const float aspectRatio = 16.0f / 9.0f;
 
     sf::Texture playerTexture;
     playerTexture.loadFromFile("Ship.png");
@@ -19,8 +20,19 @@ int main()
         // SFML 3-style event polling returns std::optional<sf::Event>
         while (auto event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>()) 
                 window.close();
+
+            if (const auto resized = event->getIf<sf::Event::Resized>()) {
+                unsigned int width = resized->size.x;
+                unsigned int height = resized->size.y;
+
+                unsigned int newHeight = static_cast<int>(width / aspectRatio);
+
+                window.setSize({width, newHeight});
+
+                window.setView(sf::View(sf::FloatRect({ 0.0f, 0.0f }, { static_cast<float>(width),  static_cast<float>(newHeight) })));
+            }
         }
 
         float dt = clock.restart().asSeconds();
