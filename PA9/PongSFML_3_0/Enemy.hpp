e1585b5c9442bcd27d108dee2544fbebf29ddd7e
4beijing <maxdsply@gmail.com>
2025-12-01 22:08:50 -0800

    enemy class

--- PA9/PongSFML_3_0/Enemy.hpp
+++ PA9/PongSFML_3_0/Enemy.hpp
@@ -1,3 +1,24 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+class Enemy : public sf::Sprite 
+{
+
+public:
+	Enemy(const sf::Texture& texture)
+		: sf::Sprite(texture)
+	{
+
+	}
+
+	void propagateEnemy()
+	{
+
+	}
+
+private:
+	//assign HP to enemy
+	int health = 50;
+
+
+};
