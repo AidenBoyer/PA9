2ac4a3461de4ab13f071c74261119014a9865f04
NomadicCat <55854868+NomadicCat@users.noreply.github.com>
2025-12-02 19:38:50 -0800

    example

--- PA9/PongSFML_3_0/WaveSystem.hpp
+++ PA9/PongSFML_3_0/WaveSystem.hpp
@@ -83,9 +83,59 @@ private:
 
 
 /*
+* Example main
+#include "WaveSystem.hpp"
+sf::RenderWindow window(sf::VideoMode({ 600, 600 }), "Space Defender");
+
+	const float aspectRatio = 16.0f / 9.0f;
+
+	sf::Texture playerTexture;
+	std::cout << std::filesystem::current_path() << std::endl;
+	playerTexture.loadFromFile("Ship.png");
+	Player player(playerTexture);
+
+	sf::Texture enemyTexture;
+	enemyTexture.loadFromFile("Enemy.png");
+	Enemy enemy1(enemyTexture);
 
 
-	waveSystem.updateLayout(window.getSize()); // Reposition and rescale enemies
+	WaveSystem waveSystem(enemyTexture);
+	waveSystem.spawnWave(0, window.getSize());
+
+	sf::Clock clock;
+	while (window.isOpen())
+	{
+		// SFML 3-style event polling returns std::optional<sf::Event>
+		while (auto event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+
+			if (const auto resized = event->getIf<sf::Event::Resized>()) {
+				unsigned int width = resized->size.x;
+				unsigned int height = resized->size.y;
+
+				unsigned int newHeight = static_cast<int>(width / aspectRatio);
+
+				window.setSize({width, newHeight});
+
+				window.setView(sf::View(sf::FloatRect({ 0.0f, 0.0f }, { static_cast<float>(width),  static_cast<float>(newHeight) })));
+				waveSystem.updateLayout(window.getSize());
+			}
+		}
+
+		float dt = clock.restart().asSeconds();
+		player.update(dt, window);
+
+
+		window.clear();
+		window.draw(player);
+		for(const auto& enemy : waveSystem.getEnemies()) {
+			window.draw(enemy);
+		}
+		window.display();
+	}
+	
 
 
 */
