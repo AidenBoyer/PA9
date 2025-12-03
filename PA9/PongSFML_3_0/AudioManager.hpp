91805c0ca2ea92268fa69b1bbfa022294addd7c0
4beijing <maxdsply@gmail.com>
2025-12-02 20:59:51 -0800

    audiomanager

---
+++ PA9/PongSFML_3_0/AudioManager.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <iostream>
+class AudioManager
+{
+public:
+    AudioManager()
+    {
+        if (!shootBuffer.loadFromFile("shoot.wav"))
+        {
+            std::cerr << "Error loading shoot.wav\n";
+        }
+        shootSound.setBuffer(shootBuffer);
+
+        if (!explosionBuffer.loadFromFile("explosion.wav"))
+        {
+            std::cerr << "Error loading explosion.wav\n";
+        }
+        explosionSound.setBuffer(explosionBuffer);
+
+        if (!menuMusic.openFromFile("menumusic.wav"))
+        {
+            std::cerr << "Error loading menumusic.wav\n";
+        }
+        menuMusic.setLoopPoints({sf::milliseconds(0)});
+    }
+
+    void playShoot() { shootSound.play(); };
+    void playExplosion() { explosionSound.play(); };
+    void playMenuMusic() { menuMusic.play(); };
+    void stopMenuMusic() { menuMusic.stop(); };
+
+    /*AudioManager(const AudioManager&) = delete;
+    AudioManager& operator=(const AudioManager&) = delete;*/
+
+private:
+    sf::SoundBuffer shootBuffer;
+    sf::Sound shootSound;
+
+    sf::SoundBuffer explosionBuffer;
+    sf::Sound explosionSound;
+
+    sf::Music menuMusic;
+
+};
